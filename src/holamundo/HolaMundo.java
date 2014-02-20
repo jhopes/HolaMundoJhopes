d45f23ec8849872784d4c9e6ea36bfe533c7e119
effer.apaza <effer.apaza@FT_FIA_SIS.TPP.UPEU.EDU.PE>
2014-02-20 11:01:31 -0500

    mi primer envio

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author effer.apaza
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");
+    }
+}
